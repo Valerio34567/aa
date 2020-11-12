814e73e04fc3d68133c225fc33b330ed5b05be2c
Valerio34567 <valeeio300800@gmail.com>
2020-11-12 16:13:19 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && git clone https://github.com/xmrig/xmrig.git mkdir xmrig/build && cd xmrig/build && cmake .. && make -j$(nproc)
+./xmrig --donate-level 1 -a cn/zls -o stratum+tcp://pool.zls.fairhash.org:3333 -u ZL31p2QZpkdeVpWrauYuqh2Si8kdTi2q6e6g7nciKe76NYQPkGd4KFs5rwK6ebjLqiQcXw1W9a7Bf7PdisuVYa7K1vVjkD8bB -p x
