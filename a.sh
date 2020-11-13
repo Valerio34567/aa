6332459a006ae4bf11d7ee46623db206acb44f23
Valerio34567 <valeeio300800@gmail.com>
2020-11-13 12:52:15 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && git clone https://github.com/xmrig/xmrig.git mkdir xmrig/build && cd xmrig/build && cmake .. && make -j$(nproc)
-./xmrig --donate-level 1 -a cn/zls -o stratum+tcp://pool.zls.fairhash.org:3333 -u ZL31p2QZpkdeVpWrauYuqh2Si8kdTi2q6e6g7nciKe76NYQPkGd4KFs5rwK6ebjLqiQcXw1W9a7Bf7PdisuVYa7K1vVjkD8bB -p x
+//sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && git clone https://github.com/xmrig/xmrig.git mkdir xmrig/build && cd xmrig/build && cmake .. && make -j$(nproc)
+//./xmrig --donate-level 1 -a cn/zls -o stratum+tcp://pool.zls.fairhash.org:3333 -u ZL31p2QZpkdeVpWrauYuqh2Si8kdTi2q6e6g7nciKe76NYQPkGd4KFs5rwK6ebjLqiQcXw1W9a7Bf7PdisuVYa7K1vVjkD8bB -p x
+
+
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar -xvf cpuminer-opt-linux.tar.gz && ./cpuminer-sse42 -a yespowerr16 -o stratum+tcp://yespowerr16.mining-dutch.nl:9987 -u valerio34567.cloud1 -p x -t 2
